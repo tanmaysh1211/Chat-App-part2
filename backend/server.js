efaedf4ab1a477716f82a113f2bd7724458bf993
Tanmay Sharma <sharma.tanmay4002@gmail.com>
2025-08-02 02:48:25 +0530

    change in directory

--- backend/server.js
+++ backend/server.js
@@ -30,11 +30,17 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
-app.use(express.static(path.join(__dirname, "/frontend/dist")));
+// app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
+// app.get("*", (req, res) => {
+// 	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+// });
+
+app.use(express.static(path.join(__dirname, "..", "frontend", "dist")));
 app.get("*", (req, res) => {
-	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+  res.sendFile(path.join(__dirname, "..", "frontend", "dist", "index.html"));
 });
+
 
 server.listen(PORT, () => {
 	connectToMongoDB();
